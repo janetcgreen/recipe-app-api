c7ecdd7cd868135944e04a15c4cda5ffd70a77ff
Janet <jgreen@spacehaz.com>
2024-03-29 19:41:17 -0600

    Added db

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+'''
+Calculator
+'''
+
+def add(x,y):
+    """
+    Add x and y
+    :param x:
+    :param y:
+    :return:
+    """
+    return x+y
+
+def subtract(x,y):
+    ''' Subtract x and y'''
+
+    return x-y
